3ac0460b432782a702048a686e94f5b43075cf85
diegodusan <128428190+diegodusan@users.noreply.github.com>
2023-07-27 19:07:28 -0500

    Proyecto con configuración inicial para compilar en android

--- lib/Config/ConfigGeneral.dart
+++ lib/Config/ConfigGeneral.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Config {
-  static const Color colorPrincipal = Color(0xFF0468BE);
-  static const Color colorTerciario = Color(0xFFD93F3F);
-  static const Color ColorSecundario = Color(0xFF353535);
-  static const Color ColorFinal = Color(0xFF3FD987);
-  static const Color graycolor = Color(0xFFD9D9D9);
-  static const Color blanco = Color(0xFFFFFFFF);
+  static const Color primary_color = Color(0xFF0528F0);
+  static const Color second_color = Color(0xFF0540F0);
+  static const Color third_color = Color(0xFF6387F2);
+  static const Color contrast_color = Color(0xFFF00505);
+  static const Color white_color = Color(0xFFF2F2F2);
+  static const Color gray_color = Color(0xFF3C3C3B);
+
+
   static const String Tema_app = "CALCULO 1";
 }
