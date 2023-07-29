c67c1d74346e80c93084d4ac78fbc17d1e22ca2b
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-07-29 10:29:07 -0500

    Proyecto estable Trabaado

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lerner_plantilla/Pages/Dashboard.dart';
 import 'package:lerner_plantilla/Pages/Temario.dart';
 import 'package:lerner_plantilla/Pages/wait_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'Config/ConfigGeneral.dart';
+import 'Objetos/Contenido.dart';
+import 'Objetos/Subtemas.dart';
+import 'Objetos/Temas.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +28,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      initialRoute: "/home",
+      initialRoute: "/",
             routes: {
         '/': (context) => const WaitScreenBuild(),
         '/home': (context) => const Dashboard(),
@@ -29,121 +38,15 @@ class MyApp extends StatelessWidget {
             builder: (context) => const WaitScreenBuild()
         );
     },
-
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        dividerColor: Colors.transparent,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  //variables
-  final db = FirebaseFirestore.instance; //inicializar firebase
-  List<Temas> temasList = [];
-
-  @override
-  void initState() {
-    super.initState();
-    obtenerTemasDesdeFirebase();
-  }
 
 
-  Future obtenerTemasDesdeFirebase() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool datosDescargados = prefs.getBool('datos_descargados_listatemas') ??
-        false;
-    if (!datosDescargados) {
-      print("Los datos apenas se van a descargar, priemra vez");
-      CollectionReference referenceTemas = FirebaseFirestore.instance
-          .collection("MATERIAS").doc(Config.Tema_app).collection("TEMAS");
-      QuerySnapshot queryTemas = await referenceTemas.get();
 
-      for (var temaDoc in queryTemas.docs) {
-        String nombreTema = temaDoc['nombre Tema'];
-        int ordenTema = temaDoc['Orden tema'];
-        print("$ordenTema $nombreTema");
-
-        //Ahora metemos subtemas para crear la lista y guardar
-        QuerySnapshot subtemasDocs = await temaDoc.reference.collection(
-            'SUBTEMAS').get();
-        List<SubTemas> subtemasList = [];
-        for (var subtemaDoc in subtemasDocs.docs) {
-          String nombreSubTema = subtemaDoc['nombreSubTema']; // Suponiendo que 'nombreSubTema' es el campo que contiene el nombre del subtema
-          int ordenSubtema = subtemaDoc['ordenSubtema'];
-          print("$ordenSubtema $nombreSubTema");
-
-          //Ahora cargamos el contenido
-          QuerySnapshot contenidoDocs = await subtemaDoc.reference.collection(
-              "CONTENIDO").get();
-
-          List<Contenido> contenidos = [];
-          List<Map<String, dynamic>> contenidoData = [];
-          for (var contenidoDoc in contenidoDocs.docs) {
-            final data = contenidoDoc.data() as Map<String, dynamic>;
-            contenidoData =
-                (data['contenido'] as List).cast<Map<String, dynamic>>();
-            Contenido contenido = Contenido(contenidoData,);
-            contenidos.add(contenido);
-            print("contenido = $contenidoData");
-          }
-          //guaramos subtemas
-          SubTemas subtema = SubTemas(nombreSubTema, ordenSubtema, contenidos);
-          subtemasList.add(subtema);
-        }
-
-        Temas tema = Temas(nombreTema, ordenTema, subtemasList);
-        temasList.add(tema);
-        print("temalist $temasList");
-      }
-      guardardatos();
-      return temasList;
-    } else {
-      print('ya descargados los datos, se cargan de sharedpreferences');
-    }
-  }
-
-  Future guardardatos() async{
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    print("se guardan datos");
-    String temasJson = jsonEncode(temasList); //convertios a Json para guardar
-    await prefs.setString('temas_list', temasJson); // Guardamos en shared preferecnes
-    await prefs.setBool('datos_descargados_listatemas', true);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(onPressed: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Temario()),
-              );
-      }, child: Text('A DASH')),
-          ],
-        ),
-      ),
-
-    );
-  }
-}
